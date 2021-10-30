0fd1470072109a3edfe8d8e538985f153558ce84
Fogrex <fogrexon@gmail.com>
2021-10-30 17:12:32 +0900

    fix tag

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 export const Tag = (
   {
     children,
-    white = true,
+    white = false,
   }: {
     children: string
     white?: boolean
   }) => (
   <span className={
-    `px-2 py-0.5 border-2 text-sm rounded-md transition-all cursor-pointer text-${white ? 'white' : 'gray-700'} border-${white ? 'white' : 'gray-700'} hover:bg-${white ? 'white' : 'gray-700'} hover:text-${!white ? 'white' : 'gray-700'}`
+    `px-2 py-0.5 mx-1 my-1 border-2 text-sm rounded-md transition-all cursor-pointer text-${white ? 'white' : 'gray-700'} border-${white ? 'white' : 'gray-700'} hover:bg-${white ? 'white' : 'gray-700'} hover:text-${!white ? 'white' : 'gray-700'}`
   }>
     { children }
   </span>
