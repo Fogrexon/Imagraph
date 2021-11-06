e8236b87d67e14af974f5c276eb81a3176fb7ce4
Fogrex <fogrexon@gmail.com>
2021-11-06 15:36:08 +0900

    fix error

--- src/components/gallery/cardlist.tsx
+++ src/components/gallery/cardlist.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WorkInfo } from '../../lib/firestore';
+import { WorkInfo } from '../../lib/types';
 import { Card } from '../ui/card';
 import items from './temp.json';
 
