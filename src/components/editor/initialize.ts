4768db446ba864d1e282c616ac015a1f9bec13b7
Fogrex <fogrexon@gmail.com>
2021-12-16 03:31:32 +0900

    seperate

---
+++ src/components/editor/initialize.ts
@@ -0,0 +1,60 @@
+/* eslint-disable no-param-reassign */
+import { Ace } from "ace-builds";
+import { DefaultInput, Filter, Renderer } from "graphim";
+import { MutableRefObject } from "react";
+
+export const initialize =
+  (
+    filterRef: MutableRefObject<Filter | undefined>,
+    rendererRef: MutableRefObject<Renderer | undefined>,
+    defaultInputRef: MutableRefObject<DefaultInput | undefined>,
+    image: HTMLImageElement,
+    glslRef: MutableRefObject<string | undefined>,
+    updateErrors: (newErrors: Ace.Annotation[]) => void
+  ) =>
+  () => {
+    rendererRef.current = new Renderer({ image });
+    // shader compile error
+    try {
+      filterRef.current = new Filter(glslRef.current as string);
+      defaultInputRef.current = new DefaultInput();
+      filterRef.current.connect(defaultInputRef.current);
+      updateErrors([]);
+    } catch (e) {
+      const errorMes: Ace.Annotation[] = `${e}`
+        .split('ERROR: ')
+        .slice(1)
+        .map((mes) => {
+          const splited = mes.split(':');
+          const row = Number(splited[1]) - 4;
+          const error = splited.slice(2).join(':');
+
+          return { row, column: 0, text: error, type: 'error' };
+        });
+      updateErrors(errorMes);
+    }
+    rendererRef.current.animate(filterRef.current as Filter);
+  };
+
+export const refreshShader = (
+  filterRef: MutableRefObject<Filter | undefined>,
+  glslRef: MutableRefObject<string | undefined>,
+  updateErrors: (newErrors: Ace.Annotation[]) => void
+) => {
+  try {
+    filterRef.current?.setShader(glslRef.current as string);
+    updateErrors([]);
+  } catch (e) {
+    const errorMes: Ace.Annotation[] = `${e}`
+      .split('ERROR: ')
+      .slice(1)
+      .map((mes) => {
+        const splited = mes.split(':');
+        const row = Number(splited[1]) - 4;
+        const error = splited.slice(2).join(':');
+
+        return { row, column: 0, text: error, type: 'error' };
+      });
+    updateErrors(errorMes);
+  }
+};
--- src/components/editor/viewer.tsx
+++ src/components/editor/viewer.tsx
@@ -3,65 +3,12 @@
 /* eslint-disable no-unused-vars */
 import { Ace } from 'ace-builds';
 import { DefaultInput, Filter, Renderer } from 'graphim';
-import React, { createRef, MutableRefObject, useEffect, useRef } from 'react';
+import React, { createRef, useEffect, useRef } from 'react';
 import { Alert } from '../ui/alert';
 import hatoImg from '../ui/hato.jpg';
+import { initialize, refreshShader } from './initialize';
 
-const initialize =
-  (
-    filterRef: MutableRefObject<Filter | undefined>,
-    rendererRef: MutableRefObject<Renderer | undefined>,
-    defaultInputRef: MutableRefObject<DefaultInput | undefined>,
-    image: HTMLImageElement,
-    glslRef: MutableRefObject<string | undefined>,
-    updateErrors: (newErrors: Ace.Annotation[]) => void
-  ) =>
-  () => {
-    rendererRef.current = new Renderer({ image });
-    // shader compile error
-    try {
-      filterRef.current = new Filter(glslRef.current as string);
-      defaultInputRef.current = new DefaultInput();
-      filterRef.current.connect(defaultInputRef.current);
-      updateErrors([]);
-    } catch (e) {
-      const errorMes: Ace.Annotation[] = `${e}`
-        .split('ERROR: ')
-        .slice(1)
-        .map((mes) => {
-          const splited = mes.split(':');
-          const row = Number(splited[1]) - 4;
-          const error = splited.slice(2).join(':');
 
-          return { row, column: 0, text: error, type: 'error' };
-        });
-      updateErrors(errorMes);
-    }
-    rendererRef.current.animate(filterRef.current as Filter);
-  };
-
-const refreshShader = (
-  filterRef: MutableRefObject<Filter | undefined>,
-  glslRef: MutableRefObject<string | undefined>,
-  updateErrors: (newErrors: Ace.Annotation[]) => void
-) => {
-  try {
-    filterRef.current?.setShader(glslRef.current as string);
-    updateErrors([]);
-  } catch (e) {
-    const errorMes: Ace.Annotation[] = `${e}`
-      .split('ERROR: ')
-      .slice(1)
-      .map((mes) => {
-        const splited = mes.split(':');
-        const row = Number(splited[1]) - 4;
-        const error = splited.slice(2).join(':');
-
-        return { row, column: 0, text: error, type: 'error' };
-      });
-    updateErrors(errorMes);
-  }
-};
 
 // eslint-disable-next-line no-shadow
 export const Viewer = ({
