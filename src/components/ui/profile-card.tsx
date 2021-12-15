53cd5f828228cafd6a71d7765036addbf212c7ac
Fogrex <fogrexon@gmail.com>
2021-12-16 01:39:08 +0900

    logout

--- src/components/ui/profile-card.tsx
+++ src/components/ui/profile-card.tsx
@@ -1,5 +1,6 @@
 import { getAuth } from 'firebase/auth';
 import React, { ReactNode, useContext } from 'react';
+import { logout } from '../../lib/auth';
 import { firebaseApp } from '../../lib/firebase';
 import { AuthContext } from '../common/auth';
 import { Actor } from './actor';
@@ -11,6 +12,10 @@ const ListItem = ({ children, small = false }: { children: ReactNode; small?: bo
 
 export const ProfileCard = () => {
   const { user } = useContext(AuthContext);
+  const sessionLogout = () => {
+    logout();
+    getAuth(firebaseApp).signOut();
+  }
   return (
     <div className="absolute top-20 right-4 w-max-full w-80 shadow bg-white rounded-md">
       <section className="w-full p-2 my-4 border-b-2">
@@ -23,7 +28,7 @@ export const ProfileCard = () => {
         </ul>
       </section>
       <section className="w-full text-center p-2 my-4">
-        <Button primary small onClick={() => getAuth(firebaseApp).signOut()}>
+        <Button primary small onClick={sessionLogout}>
           Logout
         </Button>
       </section>
